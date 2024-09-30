144b96acbb1de71cdf43638431b68ada6b386a00
Haniche Zahir <zahir.haniche@inetum.com>
2024-09-30 14:22:13 +0200

    Storing Server Actions In Separate Files

---
+++ actions/posts.js
@@ -0,0 +1,35 @@
+"use server";
+
+import { storePost } from '@/lib/posts';
+import { redirect } from 'next/navigation';
+
+export async function createPost(prevState, formData) {
+    const title = formData.get('title');
+    const image = formData.get('image');
+    const content = formData.get('content');
+
+    let errors = [];
+
+    if(!title || title.trim().length === 0) {
+      errors.push("Title is Required")
+    }
+    if(!content || content.trim().length === 0) {
+      errors.push("Content is Required")
+    }
+    if(!image || image.size === 0) {
+      errors.push("Image is Required")
+    }
+
+    if(errors.length > 0) {
+      return { errors };
+    }
+    
+    await storePost({
+      imageUrl: '',
+      title,
+      content,
+      userId: 1
+    })
+
+    redirect('/feed');
+  }
--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,41 +1,8 @@
+"use client";
+import { createPost } from '@/actions/posts';
 import PostForm from '@/components/post-form';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
-
 
 export default function NewPostPage() {
-
-  async function createPost(prevState, formData) {
-    "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-
-    let errors = [];
-
-    if(!title || title.trim().length === 0) {
-      errors.push("Title is Required")
-    }
-    if(!content || content.trim().length === 0) {
-      errors.push("Content is Required")
-    }
-    if(!image || image.size === 0) {
-      errors.push("Image is Required")
-    }
-
-    if(errors.length > 0) {
-      return { errors };
-    }
-    
-    await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
-
-    redirect('/feed');
-  }
 
   return <PostForm action={createPost} />
 }
