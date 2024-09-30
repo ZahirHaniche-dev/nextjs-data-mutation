a80b19460cd4ceba8f465cad368b1d952b015a80
Haniche Zahir <zahir.haniche@inetum.com>
2024-09-30 14:16:21 +0200

    Adjusting Server Actions for useFormState

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,17 +1,31 @@
-import FormSubmit from '@/components/form-submit';
+import PostForm from '@/components/post-form';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 
 
 export default function NewPostPage() {
 
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
-    console.log(title, image, content);
+    let errors = [];
+
+    if(!title || title.trim().length === 0) {
+      errors.push("Title is Required")
+    }
+    if(!content || content.trim().length === 0) {
+      errors.push("Content is Required")
+    }
+    if(!image || image.size === 0) {
+      errors.push("Image is Required")
+    }
+
+    if(errors.length > 0) {
+      return { errors };
+    }
     
     await storePost({
       imageUrl: '',
@@ -23,31 +37,5 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />
 }
